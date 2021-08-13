ab8bd3aa6d1538414ca285939b4327952e104a30
Lucas <lucas.maffazioli@gmail.com>
2021-08-13 22:09:38 +0200

    Responsive homepage

--- src/pages/components/homepage/index.tsx
+++ src/pages/components/homepage/index.tsx
@@ -6,31 +6,21 @@ export function HomePage() {
 	return (
 		<div className={styles.homeContainer}>
 			<div className={styles.homeContent}>
-				<table>
-					<tr>
-						<td>
-							<h2>Hey, welcome 👋</h2>
-							<br />
-							<br />
-							<h1>
-								Articles about the <span>Zen</span> world
-							</h1>
-							<br />
-							<br />
-							<h2>
-								Get acess to all the publications for <span>$9,90 month</span>
-							</h2>
-						</td>
-						<td>
-							<Image
-								src="/imgHome.svg"
-								alt="picture"
-								width={800}
-								height={1000}
-							/>
-						</td>
-					</tr>
-				</table>
+				<div>
+					<h2>Hey, welcome 👋</h2>
+					<br />
+					<br />
+					<h1>
+						Articles about the <span>Zen</span> world
+					</h1>
+					<br />
+					<h2>
+						Get acess to all the publications for <span>$9,90 month</span>
+					</h2>
+				</div>
+				<div>
+					<Image src="/imgHome.svg" alt="picture" width={800} height={1000} />
+				</div>
 			</div>
 		</div>
 	)
