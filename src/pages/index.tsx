e706ccef792cb9c3bd4e4f57085898927c76e71e
Lucas <lucas.maffazioli@gmail.com>
2021-08-13 10:29:28 +0200

    Global css and favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
-import styles from "../styles/home.module.scss";
+import Head from 'next/head'
 
 export default function Home() {
-	return <h1 className={styles.title}>lol</h1>;
+	return (
+		<>
+			<Head>
+				<title>Zen Texts</title>
+			</Head>
+			<h1>Hello World</h1>
+		</>
+	)
 }
