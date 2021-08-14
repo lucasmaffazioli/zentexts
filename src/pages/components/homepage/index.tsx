ad0e05da1572f78ab8d02d4ab182d70f8bc554aa
Lucas <lucas.maffazioli@gmail.com>
2021-08-14 11:19:44 +0200

    Responsive header

--- src/pages/components/homepage/index.tsx
+++ src/pages/components/homepage/index.tsx
@@ -1,27 +1,31 @@
+import Head from 'next/head'
 import Image from 'next/image'
 
 import styles from './styles.module.scss'
 
 export function HomePage() {
 	return (
-		<div className={styles.homeContainer}>
-			<div className={styles.homeContent}>
-				<div>
-					<h2>Hey, welcome 👋</h2>
-					<br />
-					<br />
-					<h1>
-						Articles about the <span>Zen</span> world
-					</h1>
-					<br />
-					<h2>
-						Get acess to all the publications for <span>$9,90 month</span>
-					</h2>
-				</div>
-				<div>
-					<Image src="/imgHome.svg" alt="picture" width={400} height={600} />
+		<>
+			<Head>
+				<title>Zen Texts</title>
+			</Head>
+			<div className={styles.homeContainer}>
+				<div className={styles.homeContent}>
+					<section>
+						<h2>Hey, welcome 👋</h2>
+						<br />
+						<br />
+						<h1>
+							Articles about the <span>Zen</span> world.
+						</h1>
+						<br />
+						<h2>
+							Get acess to all the publications for <span>$9,90 month</span>
+						</h2>
+					</section>
+					<Image src="/imgHome.svg" alt="picture" width={300} height={550} />
 				</div>
 			</div>
-		</div>
+		</>
 	)
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,5 @@ import Head from 'next/head'
 import { HomePage } from './components/homepage'
 
 export default function Home() {
-	return (
-		<>
-			<Head>
-				<title>Zen Texts</title>
-			</Head>
-			<HomePage />
-		</>
-	)
+	return <HomePage />
 }
