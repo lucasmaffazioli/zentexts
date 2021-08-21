6d79d1c1624d37252ca212e80be9da0e8dec9d70
Lucas <lucas.maffazioli@gmail.com>
2021-08-21 16:46:16 +0200

    Posts page

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,40 @@
+import styles from './styles.module.scss'
+
+export default function posts() {
+	return (
+		<main className={styles.container}>
+			<div className={styles.posts}>
+				<a href="">
+					<time>12 de março de 2021</time>
+					<strong>How to meditate</strong>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+						eget aliquam libero. Vestibulum tempus tincidunt ante, a mattis
+						ante. Pellentesque vestibulum ac neque eu maximus. Donec accumsan
+						dapibus tortor sed lobortis. Donec posuere hendrerit dignissim.
+					</p>
+				</a>
+				<a href="">
+					<time>12 de março de 2021</time>
+					<strong>How to meditate</strong>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+						eget aliquam libero. Vestibulum tempus tincidunt ante, a mattis
+						ante. Pellentesque vestibulum ac neque eu maximus. Donec accumsan
+						dapibus tortor sed lobortis. Donec posuere hendrerit dignissim.
+					</p>
+				</a>
+				<a href="">
+					<time>12 de março de 2021</time>
+					<strong>How to meditate</strong>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+						eget aliquam libero. Vestibulum tempus tincidunt ante, a mattis
+						ante. Pellentesque vestibulum ac neque eu maximus. Donec accumsan
+						dapibus tortor sed lobortis. Donec posuere hendrerit dignissim.
+					</p>
+				</a>
+			</div>
+		</main>
+	)
+}
