c37b835a5f1e224c871cdcd5a6944fe6524cd683
Lucas <lucas.maffazioli@gmail.com>
2021-08-25 11:30:23 +0200

    Post link is now conditional if user has permission

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,85 +1,94 @@
-import { GetStaticProps } from 'next'
-import Link from 'next/link'
-import { getPrismiscClient } from '../../services/prismic'
-import { RichText } from 'prismic-dom'
-import Prismic from '@prismicio/client'
-import styles from './styles.module.scss'
-import Head from 'next/head'
+import { GetStaticProps } from 'next';
+import Link from 'next/link';
+import { getPrismiscClient } from '../../services/prismic';
+import { RichText } from 'prismic-dom';
+import Prismic from '@prismicio/client';
+import styles from './styles.module.scss';
+import Head from 'next/head';
+import { useEffect } from 'react';
+import { useSession } from 'next-auth/client';
+import { useRouter } from 'next/dist/client/router';
 
 interface PostsProps {
-	posts: [
-		{
-			slug: string
-			title: string
-			excerp: string
-			updatedAt: Date
-		}
-	]
+  next_page: string;
+  posts: [
+    {
+      slug: string;
+      title: string;
+      excerp: string;
+      updatedAt: Date;
+    }
+  ];
 }
 
-export default function Posts({ posts }: PostsProps) {
-	function truncate(input) {
-		if (input.length > 5) {
-			return input.substring(0, 500) + '...'
-		}
-		return input
-	}
+export default function Posts({ next_page, posts }: PostsProps) {
+  const [session] = useSession();
 
-	return (
-		<>
-			<Head>
-				<title>Posts | Zen Texts</title>
-			</Head>
-			<main className={styles.container}>
-				<div className={styles.posts}>
-					{posts.map((post) => (
-						<Link key={post.slug} href={`/posts/preview/${post.slug}`}>
-							<a>
-								<time>{post.updatedAt}</time>
-								<strong>{post.title}</strong>
-								<p>{post.excerp}</p>
-							</a>
-						</Link>
-					))}
-				</div>
-				{/* <p>Pages: {documents.total_pages}</p> */}
-			</main>
-		</>
-	)
+  return (
+    <>
+      <Head>
+        <title>Posts | Zen Texts</title>
+      </Head>
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          {posts.map(post => (
+            <Link
+              key={post.slug}
+              href={
+                session?.activeSubscription
+                  ? `/posts/${post.slug}`
+                  : `/posts/preview/${post.slug}`
+              }
+            >
+              <a>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerp}</p>
+              </a>
+            </Link>
+          ))}
+        </div>
+        {next_page && <p>Load more articles</p>}
+      </main>
+    </>
+  );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-	const PrismicClient = getPrismiscClient()
-	const documents = await PrismicClient.query(
-		Prismic.predicates.at('document.type', 'post'),
-		{
-			fetch: ['publication.title', 'publication.content'],
-			pageSize: 5,
-		}
-	)
+  const PrismicClient = getPrismiscClient();
+  const documents = await PrismicClient.query(
+    Prismic.predicates.at('document.type', 'post'),
+    {
+      fetch: ['publication.title', 'publication.content'],
+      pageSize: 1,
+    }
+  );
 
-	const posts = documents.results.map((post) => {
-		return {
-			slug: post.uid,
-			title: RichText.asText(post.data.title),
-			excerp:
-				post.data.content.find((content) => content.type === 'paragraph')
-					?.text + '...' ?? '',
-			updatedAt: new Date(post.last_publication_date).toLocaleDateString(
-				'pt-BR',
-				{
-					day: '2-digit',
-					month: 'long',
-					year: 'numeric',
-				}
-			),
-		}
-	})
+  console.log(documents);
 
-	return {
-		props: {
-			posts,
-		},
-		revalidate: 60 * 60 * 2, // 2 hours
-	}
-}
+  const posts = documents.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerp:
+        post.data.content.find(content => content.type === 'paragraph')?.text +
+          '...' ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        'pt-BR',
+        {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric',
+        }
+      ),
+    };
+  });
+
+  return {
+    props: {
+      next_page: documents.next_page,
+      posts,
+    },
+    revalidate: 60 * 60 * 2, // 2 hours
+  };
+};
