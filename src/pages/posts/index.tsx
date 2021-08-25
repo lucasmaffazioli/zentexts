f916dd347ee248c8f6c59891ac3e6e9039213eec
Lucas <lucas.maffazioli@gmail.com>
2021-08-25 08:12:01 +0200

    Formatting is now SSG

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,28 +1,21 @@
 import { GetStaticProps } from 'next'
 import { getPrismiscClient } from '../../services/prismic'
-import PrismicDOM from 'prismic-dom'
+import { RichText } from 'prismic-dom'
+import Prismic from '@prismicio/client'
 import styles from './styles.module.scss'
 
 interface PostsProps {
-	documents: {
-		page: number
-		total_pages: number
-		results: [
-			{
-				id: string
-				uid: string
-				last_publication_date: Date
-				data: {
-					title
-					content
-					body
-				}
-			}
-		]
-	}
+	posts: [
+		{
+			slug: string
+			title: string
+			excerp: string
+			updatedAt: Date
+		}
+	]
 }
 
-export default function Posts({ documents }: PostsProps) {
+export default function Posts({ posts }: PostsProps) {
 	function truncate(input) {
 		if (input.length > 5) {
 			return input.substring(0, 500) + '...'
@@ -33,52 +26,13 @@ export default function Posts({ documents }: PostsProps) {
 	return (
 		<main className={styles.container}>
 			<div className={styles.posts}>
-				{documents.results.map((post) => {
-					console.log(post)
-
-					return (
-						<a key={post.uid} href="">
-							<time>
-								{Intl.DateTimeFormat('pt-BR').format(
-									new Date(post.last_publication_date)
-								)}
-							</time>
-							<strong>{PrismicDOM.RichText.asText(post.data.title)}</strong>
-							<p>{truncate(PrismicDOM.RichText.asText(post.data.content))}</p>
-						</a>
-					)
-				})}
-
-				{/* <a href="">
-					<time>12 de março de 2021</time>
-					<strong>How to meditate</strong>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-						eget aliquam libero. Vestibulum tempus tincidunt ante, a mattis
-						ante. Pellentesque vestibulum ac neque eu maximus. Donec accumsan
-						dapibus tortor sed lobortis. Donec posuere hendrerit dignissim.
-					</p>
-				</a>
-				<a href="">
-					<time>12 de março de 2021</time>
-					<strong>How to meditate</strong>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-						eget aliquam libero. Vestibulum tempus tincidunt ante, a mattis
-						ante. Pellentesque vestibulum ac neque eu maximus. Donec accumsan
-						dapibus tortor sed lobortis. Donec posuere hendrerit dignissim.
-					</p>
-				</a>
-				<a href="">
-					<time>12 de março de 2021</time>
-					<strong>How to meditate</strong>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-						eget aliquam libero. Vestibulum tempus tincidunt ante, a mattis
-						ante. Pellentesque vestibulum ac neque eu maximus. Donec accumsan
-						dapibus tortor sed lobortis. Donec posuere hendrerit dignissim.
-					</p>
-				</a> */}
+				{posts.map((post) => (
+					<a key={post.slug} href="">
+						<time>{post.updatedAt}</time>
+						<strong>{post.title}</strong>
+						<p>{post.excerp}</p>
+					</a>
+				))}
 			</div>
 			{/* <p>Pages: {documents.total_pages}</p> */}
 		</main>
@@ -87,13 +41,35 @@ export default function Posts({ documents }: PostsProps) {
 
 export const getStaticProps: GetStaticProps = async () => {
 	const PrismicClient = getPrismiscClient()
-	const documents = await PrismicClient.query('', { pageSize: 5 })
+	const documents = await PrismicClient.query(
+		Prismic.predicates.at('document.type', 'post'),
+		{
+			fetch: ['publication.title', 'publication.content'],
+			pageSize: 5,
+		}
+	)
 
-	console.log(documents)
+	const posts = documents.results.map((post) => {
+		return {
+			slug: post.uid,
+			title: RichText.asText(post.data.title),
+			excerp:
+				post.data.content.find((content) => content.type === 'paragraph')
+					?.text + '...' ?? '',
+			updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+				'pt-BR',
+				{
+					day: '2-digit',
+					month: 'long',
+					year: 'numeric',
+				}
+			),
+		}
+	})
 
 	return {
 		props: {
-			documents,
+			posts,
 		},
 		revalidate: 60 * 60 * 2, // 2 hours
 	}
