00108d7842a08e33d7d35b1ca6515961cec06113
Lucas <lucas.maffazioli@gmail.com>
2021-11-07 10:14:44 +0100

    Instalado Jest e React-testing-library

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+	testIgnorePatterns: [
+		"/.next/",
+		"node_modules",
+		"docs",
+		"dist",
+		"build",
+		"out",
+		"coverage",
+		"lib"
+	],
+	setupFilesAfterEnv: [
+		"<rootDir>/src/tests/setupTests.ts"
+	],
+	transform: {
+		"^.+\\.(js|jsx|ts|tsx)$": "babel-jest"
+	},
+	testEnvironment: 'jsdom'
+}
